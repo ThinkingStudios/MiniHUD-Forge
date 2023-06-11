26661693e3c051b3b5d590fb441af8d00632df82
TexTrue <3140846162@qq.com>
2023-06-11 14:51:56 +0800

    rename

--- src/main/java/fi/dy/masa/minihud/Reference.java
+++ src/main/java/fi/dy/masa/minihud/Reference.java
@@ -3,5 +3,5 @@ package fi.dy.masa.minihud;
 public class Reference
 {
     public static final String MOD_ID = "minihud";
-    public static final String MOD_NAME = "MiniHUD";
+    public static final String MOD_NAME = "BoccHUD";
 }
