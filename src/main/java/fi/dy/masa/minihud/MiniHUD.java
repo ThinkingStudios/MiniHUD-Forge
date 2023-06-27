a214863a8a9ca3bf8b68eb63ce23e531e80b0220
TexTrue <3140846162@qq.com>
2023-06-28 06:56:43 +0800

    1.20

--- src/main/java/fi/dy/masa/minihud/MiniHUD.java
+++ src/main/java/fi/dy/masa/minihud/MiniHUD.java
@@ -25,7 +25,6 @@ public class MiniHUD {
     public void onInitializeClient(FMLClientSetupEvent event) {
         ForgePlatformCompat.getInstance().getModClientExtensionPoint();
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-
         ForgePlatformCompat.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
             GuiConfigs gui = new GuiConfigs();
             gui.setParent(screen);
