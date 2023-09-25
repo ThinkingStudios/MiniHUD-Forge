97035b4d674add7182b808bd2fb8daa03d3b8cb7
TexTrue <3140846162@qq.com>
2023-09-25 20:48:31 +0800

    auto upload, change modid to `bocchud`

--- src/main/java/fi/dy/masa/minihud/Reference.java
+++ src/main/java/fi/dy/masa/minihud/Reference.java
@@ -2,6 +2,6 @@ package fi.dy.masa.minihud;
 
 public class Reference
 {
-    public static final String MOD_ID = "minihud";
+    public static final String MOD_ID = "bocchud";
     public static final String MOD_NAME = "BoccHUD";
 }
