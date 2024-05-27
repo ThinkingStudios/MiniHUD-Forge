e3321e4ec14e4fa42832a281f343b23a774536e4
TexTrue <3140846162@qq.com>
2024-05-27 23:11:48 +0800

    update MaFgLib version

--- src/main/java/fi/dy/masa/minihud/MiniHUD.java
+++ src/main/java/fi/dy/masa/minihud/MiniHUD.java
@@ -1,30 +1,12 @@
 package fi.dy.masa.minihud;
 
-import fi.dy.masa.malilib.compat.forge.ForgePlatformUtils;
-import fi.dy.masa.minihud.gui.GuiConfigs;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import fi.dy.masa.malilib.event.InitializationHandler;
 import fi.dy.masa.minihud.config.Configs;
 
-@Mod(Reference.MOD_ID)
 public class MiniHUD {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
     public static final String CHANNEL_CARPET_CLIENT = "CarpetClient";
-
-    public MiniHUD() {
-        if (FMLLoader.getDist().isClient()) {
-            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
-            InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-            ForgePlatformUtils.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
-                GuiConfigs gui = new GuiConfigs();
-                gui.setParent(screen);
-                return gui;
-            });
-        }
-    }
 
     public static void printDebug(String key, Object... args) {
         if (Configs.Generic.DEBUG_MESSAGES.getBooleanValue()) {
