038f2429c40aff47180993207d5c1c3aeb57bdf1
TexTrue <3140846162@qq.com>
2024-05-27 12:03:41 +0800

    update MaFgLib version

--- src/main/java/fi/dy/masa/minihud/Reference.java
+++ src/main/java/fi/dy/masa/minihud/Reference.java
@@ -1,7 +1,10 @@
 package fi.dy.masa.minihud;
 
+import fi.dy.masa.malilib.util.StringUtils;
+
 public class Reference
 {
     public static final String MOD_ID = "bocchud";
     public static final String MOD_NAME = "BoccHUD";
+    public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }
---
+++ src/main/java/org/thinkingstudio/bocchud/BoccHUD.java
@@ -0,0 +1,24 @@
+package org.thinkingstudio.bocchud;
+
+import fi.dy.masa.malilib.event.InitializationHandler;
+import fi.dy.masa.minihud.InitHandler;
+import fi.dy.masa.minihud.Reference;
+import fi.dy.masa.minihud.gui.GuiConfigs;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.thinkingstudio.mafglib.util.ForgePlatformUtils;
+
+@Mod(Reference.MOD_ID)
+public class BoccHUD {
+    public BoccHUD() {
+        if (FMLLoader.getDist().isClient()) {
+            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
+            InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+            ForgePlatformUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
+                GuiConfigs gui = new GuiConfigs();
+                gui.setParent(screen);
+                return gui;
+            });
+        }
+    }
+}
