af44e025ef6f97558324aab044ec6c36947ac8f0
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-12-21 20:01:44 +0800

    clean

--- src/main/java/org/thinkingstudio/bocchud/BoccHUD.java
+++ src/main/java/org/thinkingstudio/bocchud/BoccHUD.java
@@ -2,15 +2,20 @@ package org.thinkingstudio.bocchud;
 
 import fi.dy.masa.minihud.MiniHUD;
 import fi.dy.masa.minihud.Reference;
+import fi.dy.masa.minihud.compat.modmenu.ModMenuImpl;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(value = Reference.MOD_ID, dist = Dist.CLIENT)
 public class BoccHUD {
-    public BoccHUD() {
+    public BoccHUD(ModContainer modContainer) {
         if (FMLLoader.getDist().isClient()) {
             MiniHUD.onInitialize();
+
+            modContainer.registerExtensionPoint(IConfigScreenFactory.class, new ModMenuImpl().getModConfigScreenFactory());
         }
     }
 }
