e567460b028adb7f847da153543903ef17482b74
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-03-28 21:47:58 +0800

    clean

--- src/main/java/fi/dy/masa/minihud/renderer/OverlayRendererLightLevel.java
+++ src/main/java/fi/dy/masa/minihud/renderer/OverlayRendererLightLevel.java
@@ -2,6 +2,8 @@ package fi.dy.masa.minihud.renderer;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import fi.dy.masa.minihud.Reference;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FluidBlock;
 import net.minecraft.client.MinecraftClient;
@@ -42,7 +44,7 @@ public class OverlayRendererLightLevel extends OverlayRendererBase
 {
     public static final OverlayRendererLightLevel INSTANCE = new OverlayRendererLightLevel();
 
-    private static final Identifier TEXTURE_NUMBERS = Identifier.splitOn("minihud:textures/misc/light_level_numbers.png", ':');
+    private static final Identifier TEXTURE_NUMBERS = Identifier.splitOn(Reference.MOD_ID + ":textures/misc/light_level_numbers.png", ':');
 
     private final List<LightLevelInfo> lightInfos = new ArrayList<>();
     private final BlockPos.Mutable mutablePos = new BlockPos.Mutable();
